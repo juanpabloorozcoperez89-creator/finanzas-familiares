94d53fef3cb9b39d927d7e6ba79fa191a5313dd2
juanpabloorozcoperez89-creator <juanpabloorozcoperez89@gmail.com>
2026-05-27 09:22:28 -0600

    Update 3_Gastos_Hormiga.py

--- pages/3_Gastos_Hormiga.py
+++ pages/3_Gastos_Hormiga.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_title="Gastos Hormiga · Finanzas",
     page_icon="🐜",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="auto",
 )
 apply_theme()
 
