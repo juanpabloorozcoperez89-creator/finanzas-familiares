ee43860c1a273cc49420dfea2f0f41c98168517e
juanpabloorozcoperez89-creator <juanpabloorozcoperez89@gmail.com>
2026-05-27 09:22:40 -0600

    Update 4_Historico.py

--- pages/4_Historico.py
+++ pages/4_Historico.py
@@ -15,7 +15,7 @@ st.set_page_config(
     page_title="Histórico · Finanzas",
     page_icon="📈",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="auto",
 )
 apply_theme()
 
